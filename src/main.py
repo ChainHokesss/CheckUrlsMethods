7d9948b4d0a7b6b86acf844a5ccd87d8bfda6d08
ChainHokes <k.nazar2003@gmail.com>
2022-11-18 00:38:12 +0300

    type hints added

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import re
 import httpx
 import asyncio
 import time
+from typing import Union
 
 
 url_methods = [
@@ -28,21 +29,21 @@ value_list = [
 ]
 
 
-def url_validator(url):
+def url_validator(url: str) -> Union[str, None]:
     val_urls = re.match(url_pattern, url)
     if val_urls:
         return url
     return None
 
 
-async def check_methods(method, url):
+async def check_methods(method: str, url: str) -> Union[tuple, None]:
     async with httpx.AsyncClient() as client:
         response = await client.request(method, url)
         if response.status_code != 405:
             return method, response.status_code
 
 
-async def get_available_methods(url, methods_list):
+async def get_available_methods(url: str, methods_list: list) -> Union[tuple, None]:
     validated_url = url_validator(url)
     if validated_url is None:
         print(f'{url} is not url')
@@ -52,7 +53,7 @@ async def get_available_methods(url, methods_list):
     return url, dict(methods)
 
 
-async def main(url_list, methods_list):
+async def main(url_list: list, methods_list: list) -> dict:
     start_time = time.time()
     url_methods_cont = tuple(await asyncio.gather(*(get_available_methods(url, methods_list) for url in url_list)))
     url_methods_cont = tuple(filter(lambda x: x is not None, url_methods_cont))
